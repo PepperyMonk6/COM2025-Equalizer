3495440023a01ef1285772ea7855a8f280ab3df6
Illya Globa <ig00225@surrey.ac.uk>
2021-11-29 19:02:10 +0000

    bootstrap upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#home'
+  get 'response', to: 'home#responsive'
   resources :playlists
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
