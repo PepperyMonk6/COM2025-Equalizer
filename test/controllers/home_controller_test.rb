c58c1af84599e42796a2f0743556bf56609d0021
Illya Globa <ig00225@surrey.ac.uk>
2021-11-30 15:19:43 +0000

    contact_us page added

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,36 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
+
+    assert_select 'title', 'Equalizer'
   end
+
+    test "should get contact" do
+      get contact_url
+      assert_response :success
+
+      assert_template layout: 'application'
+
+      assert_select 'title', 'Equalizer'
+      assert_select 'h1', 'Contact Us'
+      assert_select 'p', 'Complete the following form to get in touch with us.'
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+    test "should post request contact" do
+      post request_contact_url, params:
+      {name: "Matthew", email: "matthew@me.com",
+      telephone: "1234567890", message: "Hello"}
+      
+      assert_response :redirect
+      assert_nil flash[:alert]
+      assert_not_empty flash[:notice]
+    end
 
 end
